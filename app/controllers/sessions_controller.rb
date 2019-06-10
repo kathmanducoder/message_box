dfafd71739eae87dd12cc5a9f82fe3c7abbd6afb
kathmanducoder <kathmanducoder@gmail.com>
2019-06-10 15:28:38 -0400

    Add ability to login/logout.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+    render partial: 'sessions/login'
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if !user
+      flash[:error] = "User not found."
+      redirect_to login_path
+    elsif !user.authenticate(params[:password])
+      flash[:error] = "Incorrect password."
+      redirect_to login_path
+    else
+      session[:user_id] = user.id
+      redirect_to user
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session.delete :user_id
+    end
+    redirect_to login_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
