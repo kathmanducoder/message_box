5759c5c52149d9436f378826bd338367be9c1ccd
kathmanducoder <kathmanducoder@gmail.com>
2019-06-11 18:52:19 -0400

    Add code to retrieve sent messages via Ajax

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -30,16 +30,29 @@ $(function(){
     e.preventDefault();
   })
 
+
   $("a.load_outbox").on("click", function(e) {
     $.ajax(
       {
         url: this.href,
         success: function(json) {
+          let moreMessageHref = '<a id="more_message" href="/messages/';
           json.forEach(function(message) {
-            $("div#outbox").append(message.recipient.name)
-            $("div#outbox").append("  |  ")
-            $("div#outbox").append(message.content)
-            $("div#outbox").append("  |  ")
+            $("div#outbox").append(message.sender.name)
+            $("div#outbox").append("<br>")
+            $("div#outbox").append
+            (`<div id="body-${message.id}">` + ($.trim(message.content).substring(0,10).trim(this)) + '</div>');
+            moreMessageHref = moreMessageHref + message.id + '/content">...</a>';
+            let moreMessageLink = $(moreMessageHref).click(function(e) {
+                $.get("/messages/" + message.id + "/content", function(data) {
+                  $("div#body-"+message.id).text(data);
+                });
+                e.preventDefault();
+              });
+            $("div#outbox").append(moreMessageLink)
+            $("div#outbox").append("</div>")
+
+            $("div#outbox").append("<br>")
             $("div#outbox").append(message.created_at);
             $("div#outbox").append("<br>")
           })
@@ -47,6 +60,5 @@ $(function(){
       })
     e.preventDefault();
   })
-
 
 })
