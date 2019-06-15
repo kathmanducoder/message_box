4e92cf85eacb6a2d527febe02343765d8cd3b532
kathmanducoder <kathmanducoder@gmail.com>
2019-06-15 14:48:40 -0400

    Add seed data

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_06_10_195914) do
   create_table "messages", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
-    t.string "subject"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seed Data
+
+# Users
+User.create(name: "Anup Khadka", username: "anup", password: "anup")
+User.create(name: "George Costanza", username: "guest", password: "guest")
+User.create(name: "Jerry Seinfeld", username: "jerry", password: "jerry")
+User.create(name: "Elaine Benes", username: "elaine", password: "elaine")
+User.create(name: "Cosmo Kramer", username: "cosmo", password: "cosmo")
+User.create(name: "Newman", username: "newman", password:"newman")
+
+
+# Messages
+Message.create(sender_id: 2, recipient_id: 3, content: "The sea was angry that day, my friends.")
+Message.create(sender_id: 2, recipient_id: 4, content: "The sea was angry that day, my friends.")
+Message.create(sender_id: 2, recipient_id: 5, content: "The sea was angry that day, my friends.")
+Message.create(sender_id: 2, recipient_id: 6, content: "The sea was angry that day, my friends.")
+
+Message.create(sender_id: 3, recipient_id: 2, content: "I had a dream last night that a hamburger was eating me")
+Message.create(sender_id: 3, recipient_id: 4, content: "I had a dream last night that a hamburger was eating me")
+Message.create(sender_id: 3, recipient_id: 5, content: "I had a dream last night that a hamburger was eating me")
+Message.create(sender_id: 3, recipient_id: 6, content: "I had a dream last night that a hamburger was eating me")
+
+Message.create(sender_id: 3, recipient_id: 2, content: "Boy, a little too much chlorine in that gene pool")
+Message.create(sender_id: 3, recipient_id: 4, content: "Boy, a little too much chlorine in that gene pool")
+Message.create(sender_id: 3, recipient_id: 5, content: "Boy, a little too much chlorine in that gene pool")
+Message.create(sender_id: 3, recipient_id: 6, content: "Boy, a little too much chlorine in that gene pool")
+
+
+Message.create(sender_id: 4, recipient_id: 2, content: "Yada yada yada")
+Message.create(sender_id: 4, recipient_id: 3, content: "Yada yada yada")
+Message.create(sender_id: 4, recipient_id: 5, content: "Yada yada yada")
+Message.create(sender_id: 4, recipient_id: 6, content: "Yada yada yada")
+
+Message.create(sender_id: 5, recipient_id: 2, content: "Who turns down a junior mint?")
+Message.create(sender_id: 5, recipient_id: 3, content: "Who turns down a junior mint?")
+Message.create(sender_id: 5, recipient_id: 4, content: "Who turns down a junior mint?")
+Message.create(sender_id: 5, recipient_id: 6, content: "Who turns down a junior mint?")
+
+Message.create(sender_id: 5, recipient_id: 2, content: "Giddy up!!!")
+Message.create(sender_id: 5, recipient_id: 3, content: "Giddy up!!!")
+Message.create(sender_id: 5, recipient_id: 4, content: "Giddy up!!!")
+Message.create(sender_id: 5, recipient_id: 6, content: "Giddy up!!!")
+
+
+Message.create(sender_id: 6, recipient_id: 2, content: "Your day of reckoning is coming")
+Message.create(sender_id: 6, recipient_id: 3, content: "Your day of reckoning is coming")
+Message.create(sender_id: 6, recipient_id: 4, content: "Your day of reckoning is coming")
+Message.create(sender_id: 6, recipient_id: 5, content: "Your day of reckoning is coming")
